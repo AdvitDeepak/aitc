1a6aa6e02cb5ec4ed03cf4c71a74786a8bf6a850
JadelynnDao <jazzly123@gmail.com>
2019-04-04 20:52:10 -0700

    new

---
+++ setup_conda_env.bat
@@ -0,0 +1,9 @@
+
+conda update conda
+conda update anaconda
+conda update scikit-learn
+conda install theano
+conda install -c conda-forge tensorflow
+pip install keras
+pip install pygame
+pip install serial
